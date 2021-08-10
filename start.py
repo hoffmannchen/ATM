75998160797ba1173ba5d72d9982bdf8b30b4650
Hoffmann Chen <hoffmannchen@gmail.com>
2021-08-10 11:36:02 +0800

    初步结构

--- start.py
+++ start.py
@@ -1,4 +1,14 @@
-from conf import src
+"""
+程序入口
+"""
+import sys
+import os
+from core import src
 
+# 添加解释权环境变量
+sys.path.append(os.path.dirname(__file__))
+
+# 开始执行项目函数
 if __name__ == '__main__':
+    # 1、先执行用户视图层
     src.run()
