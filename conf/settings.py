2ffea6e59927e8d947fa716bcb0aa0967396cc91
Hoffmann Chen <hoffmannchen@gmail.com>
2021-08-11 14:36:41 +0800

    分层

--- conf/settings.py
+++ conf/settings.py
@@ -7,4 +7,3 @@ import os
 BASE_PATH = os.path.dirname(os.path.dirname(__file__))
 # 获取user_data文件夹目录路径
 USER_DATA_PATH = os.path.join(BASE_PATH, 'db', 'user_data')
-print(USER_DATA_PATH)
--- interface/user_interface.py
+++ interface/user_interface.py
@@ -2,3 +2,21 @@
 逻辑接口层
     用户接口
 """
+import json
+import os
+
+
+# 注册接口
+def register_interface(username, password, balance=15000):
+    # 接收到注册后的结果
+
+    user_dict = {'username': username,
+                 'password': password,
+                 'balance': balance,
+                 'flow': [],
+                 'shop_cart': {},
+                 'locked': False
+                 }
+
+    with open(user_path, 'w', encoding='utf-8') as f:
+        json.dump(user_dict, f, ensure_ascii=False)
