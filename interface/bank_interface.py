2a09a0bda300da9675ca69043337b429a4c5d9a5
Hoffmann Chen <hoffmannchen@gmail.com>
2021-08-12 16:10:49 +0800

    提现功能100%

--- interface/bank_interface.py
+++ interface/bank_interface.py
@@ -1,7 +1,17 @@
 """
 银行相关业务接口
 """
+from db import db_handler
 
 
-def withdraw_interface(username, withdraw_money):
-    pass
+def withdraw_interface(username, money):
+    user_dict = db_handler.select(username)
+    balance = int(user_dict.get('balance'))
+    money2 = money * 1.05
+    print(f'用户{username}账户余额为{balance}')
+    if money2 > balance:
+        return False, '余额不足'
+    balance -= money2
+    user_dict['balance'] = balance
+    db_handler.save(user_dict)
+    return True, f'用户{username}提现金额{money}成功，手续费为{money * 0.05},现余额为{balance}'
